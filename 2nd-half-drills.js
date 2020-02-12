7999a9eb1e533c26d41996241d4dd75bee4069d8
Brannen Petit <bpetit940@gmail.com>
2020-02-11 20:09:46 -0600

    completed drill 6

--- 2nd-half-drills.js
+++ 2nd-half-drills.js
@@ -21,9 +21,44 @@ const softwareEngi = [
 ] 
   
 for (const obj in softwareEngi) {
-    if (softwareEngi[obj].boss === undefined){
-        console.log(`"${softwareEngi[obj].jobTitle} ${softwareEngi[obj].name} doesn't report to anybody."`)
-    } else {
-  console.log(`"${softwareEngi[obj].jobTitle} ${softwareEngi[obj].name} reports to ${softwareEngi[obj].boss}."`);
+  if (softwareEngi[obj].boss === undefined){
+    console.log(`"${softwareEngi[obj].jobTitle} ${softwareEngi[obj].name} doesn't report to anybody."`)
+  } else {
+    console.log(`"${softwareEngi[obj].jobTitle} ${softwareEngi[obj].name} reports to ${softwareEngi[obj].boss}."`);
+  }
 }
+
+// objects exercise 6
+
+function decode(word) {
+  const cipher = {
+    'a': word.charAt(1),
+    'b': word.charAt(2),
+    'c': word.charAt(3),
+    'd': word.charAt(4)
+  }
+  const string = ' ';
+  for (const key in cipher) {
+    if (key === word[0]){
+      return cipher[key];
+    }
+        
+  }
+  return string;
 }
+
+console.log(decode('abera'));
+
+function decodeWords(words){
+  const newArr = words.split(' ');
+  console.log(newArr);
+  let result = [];
+  for( let i = 0; i < newArr.length; i++) {
+    result.push(decode(newArr[i]));
+    console.log(result);
+  }
+  let result1 = result.join('');
+  console.log(result1)
+}
+
+decodeWords('craft block argon meter bells brown croon droop');
